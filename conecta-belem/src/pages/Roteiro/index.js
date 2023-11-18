5926dab8ded1330209d5cada8a220c18c2e1f81e
Amanda Lima Reis <amandalreis@me.com>
2023-11-18 18:00:38 -0300

    commit rotas iniciais

---
+++ conecta-belem/src/pages/Roteiro/index.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export function Roteiro() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.backgroundImage}>
+      <Image
+        source={require("../../assets/background-estacao-das-docas.png")}
+      />
+      </View>
+    </View>
+
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
