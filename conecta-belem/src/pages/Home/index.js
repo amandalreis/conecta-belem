ddc47ac30e82676bc02ac6c9d1df61773c40af28
Amanda Lima Reis <amandalreis@me.com>
2023-11-18 21:47:39 -0300

    a

--- conecta-belem/src/pages/Home/index.js
+++ conecta-belem/src/pages/Home/index.js
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Text>Olá</Text>
     </View>
-
-    
   );
 }
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'contain',
+    // outras estilizações conforme necessário
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
--- conecta-belem/src/pages/Inicio/index.js
+++ conecta-belem/src/pages/Inicio/index.js
@@ -11,46 +11,39 @@ export function Inicio() {
     source={require('../../assets/background-estacao-das-docas.png')}
     style={styles.backgroundImage}
     >
+
     <View style={styles.container}>
-      <Text style={[styles.text, { color: "white" }]}>O melhor roteiro para uma viagem inesquecível</Text>
       <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.button}>
         <Text style={styles.buttonText}>Avançar</Text>
       </TouchableOpacity>
     </View>
   </ImageBackground>
+
+
   );
 }
 
   const styles = StyleSheet.create({
     backgroundImage: {
       flex: 1,
-      resizeMode: 'cover',
+      resizeMode: 'contain',
       // outras estilizações conforme necessário
     },
     container: {
       flex: 1,
-      backgroundColor: 'rgba(0, 0, 0, 0.6)',
       justifyContent: 'center',
       alignItems: 'center',
-    },
-    text: {
-      fontSize: 44,
-      fontWeight: 'bold',
-      color: 'white',
-      width: 350,
-      textAlign: 'center',
-      // outras estilizações conforme necessário
     },
     button: {
       backgroundColor: '#028043',
       paddingVertical: 20,
       paddingHorizontal: 30,
-      borderRadius: 5,
-      marginTop: 50,
+      borderRadius: 8,
+      marginTop: 450,
     },
     buttonText: {
       fontSize: 18,
       color: '#fff',
-      fontWeight: 'bold',
+      fontWeight: 'regular',
     },
   });
