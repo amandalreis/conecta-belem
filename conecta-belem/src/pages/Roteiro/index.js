e3c28905cf02fb2e62683634c5428957f5cb0fac
Reynan Vieira <reynanvt@gmail.com>
2023-11-19 08:39:51 -0300

    adicionando widgets

--- conecta-belem/src/pages/Roteiro/index.js
+++ conecta-belem/src/pages/Roteiro/index.js
@@ -1,8 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
 import SelectDropdown from 'react-native-select-dropdown'
 import React, { useState } from 'react';
 
+import BackgroundAcai from '../../assets/background-acai.png';
+import BackgroundEstacaoDasDocas from '../../assets/background-estacao-das-docas.png';
+
 export function Roteiro() {
+  const dataList = [
+    {
+      id: '1',
+      image: require('../../assets/background-acai.png'), 
+      description: '08:00',
+    },
+    {
+      id: '2',
+      image: require('../../assets/background-estacao-das-docas.png'), 
+      description: '10:00',
+    },
+    // Adicione mais itens conforme necessário
+  ];
   const countries = ["21 de Dezembro", "22 de Dezembro", "23 de Dezembro", "24 de Dezembro"]
   const [selectedItem, setSelectedItem] = useState(countries[0]); // Define o primeiro item como selecionado inicialmente
 
@@ -11,6 +27,7 @@ export function Roteiro() {
       <View style={styles.top}>
         <Text style={styles.text}>Planejamento</Text>
         <SelectDropdown
+          style={styles.menu}
           data={countries}
           onSelect={(selectedItem, index) => {
             setSelectedItem(selectedItem); // Atualiza o estado com o item selecionado
@@ -22,6 +39,16 @@ export function Roteiro() {
             return item; // Define o texto exibido para cada item na lista suspensa
           }}
           defaultButtonText={selectedItem} // Define o texto do botão com o item selecionado
+        />
+        <FlatList
+          data={dataList}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+              <Image source={item.image} style={styles.image} />
+              <Text style={styles.description}>{item.description}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
         />
       </View>
     </View>
@@ -42,8 +69,28 @@ const styles = StyleSheet.create({
   },
   top: {
     alignItems: 'flex-start',
-    paddingBottom: 550,
-    paddingRight: 70,
+    paddingBottom: 20,
     marginTop: 50, 
   },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingRight: 200,
+    marginTop: 20,
+    backgroundColor: 'white',
+    borderRadius: 15,
+  },
+  image: {
+    width: 80,
+    height: 110,
+    marginRight: 16,
+    borderRadius: 10,
+  },
+  description: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  menu: {
+    borderRadius: 15,
+  }
 });
