35234dbb91bb9f028d77130df412d81620df1848
Amanda Lima Reis <amandalreis@me.com>
2023-11-19 15:05:49 -0300

    commit

--- conecta-belem/src/pages/Roteiro/index.js
+++ conecta-belem/src/pages/Roteiro/index.js
@@ -13,6 +13,7 @@ export function Roteiro() {
       image: require('../../assets/basilica.jpeg'), 
       title: '08:00 - Basílica Nossa Senhora de Nazaré',
       descricao_atividade: 'Lorem Ipsum',
+      screen: require('../../assets/roteiro-basilica.png'),
     },
     {
       id: '2',
@@ -20,12 +21,14 @@ export function Roteiro() {
       title: '09:00 - Uber Agendado',
       uber: 'Motorista: Wesley Souza',
       descricao_atividade: 'Lorem Ipsum',
+      screen: require('../../assets/roteiro-uber-wesley.png'),
     },
     {
       id: '3',
       image: require('../../assets/ver-o-peso.jpg'), 
       title: '09:30 - Ver-o-Peso',
       descricao_atividade: 'Lorem Ipsum',
+      screen: require('../../assets/roteiro-ver-o-peso.png'),
     },
     {
       id: '4',
@@ -33,12 +36,14 @@ export function Roteiro() {
       title: '11:30 - Uber Agendado',
       uber: 'Motorista: Samuel Novaes',
       descricao_atividade: 'Lorem Ipsum',
+      screen: require('../../assets/roteiro-uber-samuel.png'),
     },
     {
       id: '5',
       image: require('../../assets/logo-casa-saulo.jpg'), 
       title: '12:00 - Casa do Saulo Onze Janelas',
       descricao_atividade: 'Lorem Ipsum',
+      screen: require('../../assets/roteiro-casa-saulo.png'),
     },
     // Adicione mais itens conforme necessário
   ];
@@ -71,7 +76,7 @@ export function Roteiro() {
           ListHeaderComponent={headerRoteiro}
           data={dataList}
           renderItem={({ item }) => (
-            <TouchableOpacity onPress={() => navigation.navigate("DetalhesAtividade", { ItemId: item.id, Nome: item.title, Foto: item.image, Descricao: item.descricao_atividade })}>
+            <TouchableOpacity onPress={() => navigation.navigate("DetalhesAtividade", { Screen: item.screen })}>
             <View style={styles.item}>
               <Image source={item.image} style={styles.image} />
               <View>
